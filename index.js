d3c5600dd64fff0921c15263528a664132ddb317
Florian Jost <florian.jost89@gmail.com>
2019-08-23 17:23:39 +0200

    fix: change port

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is listening on port ${PORT }🤘`);
+  console.log(`Server is listening on port ${PORT}🤘`);
 });
