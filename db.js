1d69df2858beb80f189c1c959be84a613e82a02c
Florian Jost <florian.jost89@gmail.com>
2019-08-24 10:11:26 +0200

    connect to mongo atlas

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // eslint-disable-next-line no-console
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/karma', { useNewUrlParser: true }, () => console.log('Database connected'));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/karma', { useNewUrlParser: true }).then(() => console.log('Connected')).catch(error => console.log(error));
 
 
 module.exports = mongoose;
